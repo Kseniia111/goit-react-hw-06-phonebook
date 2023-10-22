ad62e36bfd329cc6c077a10a844c5fbdc995c12c
Kseniia111 <kseniya.markovych@gmail.com>
2023-10-22 16:54:18 +0300

    mentor's store copy

---
+++ src/redux/store.js
@@ -0,0 +1,35 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import { postDetailsReducer } from './postDetailReducer';
+
+const postDetailsConfig = {
+  key: 'postDetails',
+  storage,
+  whitelist: ['posts'],
+  //   blacklist: ['filter'],
+};
+
+export const store = configureStore({
+  reducer: {
+    postDetails: persistReducer(postDetailsConfig, postDetailsReducer),
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
